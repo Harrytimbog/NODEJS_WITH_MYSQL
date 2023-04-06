aad889da6e60b25090e37495ce10628c7fc35878
Timilehin Arigbede <harriemannie@gmail.com>
2023-04-07 00:13:03 +0100

    Connecting to the Database

--- util/database.js
+++ util/database.js
@@ -1,11 +1,22 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+// require("dotenv").config();
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: process.env.DATABASE_PASSWORD,
-});
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: process.env.DATABASE_PASSWORD,
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  "node-complete",
+  "root",
+  process.env.DATABASE_PASSWORD,
+  { dialect: "mysql", host: "localhost" }
+);
+
+module.exports = sequelize;
