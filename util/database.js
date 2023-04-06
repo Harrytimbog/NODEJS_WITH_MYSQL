b24f6b59aef5db8fd2143829502c26cb31a328cf
Timilehin Arigbede <harriemannie@gmail.com>
2023-04-07 00:35:30 +0100

    Syncing JS Definitions to the Database

--- util/database.js
+++ util/database.js
@@ -1,15 +1,4 @@
-// require("dotenv").config();
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "node-complete",
-//   password: process.env.DATABASE_PASSWORD,
-// });
-
-// module.exports = pool.promise();
-
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
